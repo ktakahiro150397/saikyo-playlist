28ae5baf73f5f3d4ea0ae05bcacd63105198551c
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2022-06-04 11:46:43 +0900

    test-branchのテストコミット

--- saikyo-playlist/Models/ErrorViewModel.cs
+++ saikyo-playlist/Models/ErrorViewModel.cs
@@ -2,6 +2,10 @@ namespace saikyo_playlist.Models
 {
     public class ErrorViewModel
     {
+
+        //test-branch‚ÌƒRƒƒ“ƒg’Ç‰Á
+
+
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
