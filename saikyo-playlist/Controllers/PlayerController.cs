70f311da606f88f54b0d26d26b98fad94e091a00
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2022-06-04 21:18:31 +0900

    プレイリストモデル作成・Youtubeル再生実装

--- saikyo-playlist/Controllers/PlayerController.cs
+++ saikyo-playlist/Controllers/PlayerController.cs
@@ -8,29 +8,11 @@ namespace saikyo_playlist.Controllers
     [Authorize]
     public class PlayerController : Controller
     {
-        public PlayerPlayListModel PlayListModel { get; set; }
-
-        public PlayerController()
-        {
-            PlayListModel = new PlayerPlayListModel();
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
+            var PlayListModel = new PlayListModel();
             return View(PlayListModel);
         }
-
-        [HttpGet]
-        public JsonResult GetNextPlayVideoId()
-        {
-
-            //再生中インデックスを加算し、次に再生する動作IDを返す
-            var nextIndex = PlayListModel.GetNextVideoIndexAndIncrementIndex();
-            return Json(new { nextId=PlayListModel.VideoId[nextIndex].Id });
-
-        }
-
-
     }
 }
