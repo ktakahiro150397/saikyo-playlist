b554b87fa5ba98132d6e7b705567d40d1acc0d25
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2022-06-04 18:10:48 +0900

    とりあえずiframe Youtube APIでの再生

---
+++ saikyo-playlist/Controllers/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using saikyo_playlist.Models;
+
+namespace saikyo_playlist.Controllers
+{
+
+    [Authorize]
+    public class PlayerController : Controller
+    {
+        public PlayerPlayListModel PlayListModel { get; set; }
+
+        public PlayerController()
+        {
+            PlayListModel = new PlayerPlayListModel();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(PlayListModel);
+        }
+
+        [HttpGet]
+        public JsonResult GetNextPlayVideoId()
+        {
+
+            //再生中インデックスを加算し、次に再生する動作IDを返す
+            var nextIndex = PlayListModel.GetNextVideoIndexAndIncrementIndex();
+            return Json(new { nextId=PlayListModel.VideoId[nextIndex].Id });
+
+        }
+
+
+    }
+}
