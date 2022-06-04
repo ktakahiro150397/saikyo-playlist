6979d31e390458ed23187becb9d9469c26fc92c1
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2022-06-04 09:04:51 +0900

    ASP.NET Identity スキャフォールドファイルの追加

--- saikyo-playlist/Program.cs
+++ saikyo-playlist/Program.cs
@@ -3,9 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using saikyo_playlist.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
