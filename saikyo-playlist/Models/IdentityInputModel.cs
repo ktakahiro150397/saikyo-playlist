40c5336128b485d7a5fb8924b1d2907c33837826
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2022-06-04 15:23:51 +0900

    アカウント新規作成画面追加

---
+++ saikyo-playlist/Models/IdentityInputModel.cs
@@ -0,0 +1,38 @@
+﻿#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+
+namespace saikyo_playlist.Models
+{
+
+    /// <summary>
+    /// アカウント新規作成時の入力データモデル
+    /// </summary>
+    public class IdentityInputCreateNewModel
+    {
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        [Required]
+        [Display(Name = "ユーザー名")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// パスワード確認
+        /// </summary>
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード確認")]
+        [Compare(nameof(Password),ErrorMessage = "入力されたデータがパスワードと一致しません。")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
--- saikyo-playlist/Program.cs
+++ saikyo-playlist/Program.cs
@@ -5,12 +5,6 @@ using saikyo_playlist.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));;
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();;
-
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -19,6 +13,19 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
+//asp.net IdentityÇÃê›íË
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+
+});
+
 
 var app = builder.Build();
 
@@ -33,6 +40,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
