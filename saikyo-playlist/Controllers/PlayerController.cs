47edfbfc0d2ddf8a07e0e751d3c2abd01ebec5c3
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2022-06-07 23:01:43 +0900

    サーバー側API追加

--- saikyo-playlist/Controllers/PlayerController.cs
+++ saikyo-playlist/Controllers/PlayerController.cs
@@ -32,5 +32,38 @@ namespace saikyo_playlist.Controllers
             return View("Index", playListModel);
         }
 
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult AddPlayCount(string playListHeaderId,int itemSeq)
+        {
+
+            try
+            {
+                var detail = ApplicationDbContext.PlayListDetails
+                        .Where(item => item.PlayListHeadersEntityId == playListHeaderId && item.ItemSeq == itemSeq)
+                        .FirstOrDefault();
+
+                if(detail == null)
+                {
+                    //データが見つからなかった
+                    return NotFound();
+                }
+
+                //データの再生回数を1つ増やす
+                detail.PlayCount += 1;
+                ApplicationDbContext.SaveChanges();
+
+            }catch(Exception ex)
+            {
+                //内部エラー
+                return BadRequest();
+            }
+
+            return Ok();
+
+        }
+
     }
 }
