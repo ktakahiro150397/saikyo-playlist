9a2e1824c4cbe11fc34b469ca5404ca6bef70bb3
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2022-06-08 20:38:25 +0900

    DbContextの追加

--- saikyo-playlist/Data/ApplicationDbContext.cs
+++ saikyo-playlist/Data/ApplicationDbContext.cs
@@ -6,7 +6,19 @@ namespace saikyo_playlist.Data
     public class ApplicationDbContext : IdentityDbContext
     {
 
+        /// <summary>
+        /// アイテムライブラリ。
+        /// </summary>
+        public DbSet<ItemLibrariesEntity> ItemLibraries => Set<ItemLibrariesEntity>();
+        
+        /// <summary>
+        /// プレイリストヘッダー。
+        /// </summary>
         public DbSet<PlayListHeadersEntity> PlayListHeaders => Set<PlayListHeadersEntity>();
+        
+        /// <summary>
+        /// プレイリスト詳細。
+        /// </summary>
         public DbSet<PlayListDetailsEntity> PlayListDetails => Set<PlayListDetailsEntity>();
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
