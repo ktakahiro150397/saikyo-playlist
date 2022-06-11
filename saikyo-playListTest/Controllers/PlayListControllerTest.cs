6a09314f144e9f762dba93f01243346b080e0843
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2022-06-11 14:33:53 +0900

    Add AddItem Invalid Test

--- saikyo-playListTest/Controllers/PlayListControllerTest.cs
+++ saikyo-playListTest/Controllers/PlayListControllerTest.cs
@@ -82,6 +82,130 @@ namespace saikyo_playListTest.Controllers
 
         }
 
+        /// <summary>
+        /// アイテムライブラリへの追加画面：POST・失敗・URLなし
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task AddItem_ReturnAViewWithInvalidModelState_NoUrl()
+        {
+            //Arrange
+            var store = new Mock<IUserStore<IdentityUser>>();
+            var userManagerMoq = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+            var configMoq = new Mock<IConfiguration>();
+            var itemLibRepo = new Mock<IItemLibraryRepository>();
+            itemLibRepo.Setup(repo => repo.InsertAsync(It.IsAny<LibraryItemPlatform>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IdentityUser>()))
+                .Verifiable();
+            var youtubeRepo = new Mock<IYoutubeDataRepository>();
+            youtubeRepo.Setup(repo => repo.GetYoutubeVideoInfoAsync(It.IsAny<string>()))
+                .ReturnsAsync(new YoutubeVideoRetrieveResult() { ItemId = "moq" })
+                .Verifiable();
+
+            var controller = new PlayListController(
+                userManagerMoq.Object,
+                itemLibRepo.Object,
+                youtubeRepo.Object,
+                configMoq.Object);
+
+            var model = new AddItemViewModel()
+            {
+                Url = "",
+                TitleAlias = "テスト",
+                Platform = LibraryItemPlatform.Youtube,
+                ErrorMessage = ""
+            };
+            controller.ModelState.AddModelError("Url", "required");
+
+            //Act
+            var actResult = await controller.AddItem(model);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(actResult);
+            var modelResult = Assert.IsAssignableFrom<AddItemViewModel>(viewResult.Model);
+            Assert.Equal("URLを入力してください。", modelResult.ErrorMessage);
+           
+        }
+
+        /// <summary>
+        /// アイテムライブラリへの追加画面：POST・失敗・動画URLからYoutubeデータ取得不可
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task AddItem_ReturnAViewWithInvalidModelState_NoDataFromVideoUrl()
+        {
+            //Arrange
+            var store = new Mock<IUserStore<IdentityUser>>();
+            var userManagerMoq = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+            var configMoq = new Mock<IConfiguration>();
+            var itemLibRepo = new Mock<IItemLibraryRepository>();
+            var youtubeRepo = new Mock<IYoutubeDataRepository>();
+            youtubeRepo.Setup(repo => repo.GetYoutubeVideoInfoAsync(It.IsAny<string>()))
+                .Verifiable();
+
+            var controller = new PlayListController(
+                userManagerMoq.Object,
+                itemLibRepo.Object,
+                youtubeRepo.Object,
+                configMoq.Object);
+
+            var model = new AddItemViewModel()
+            {
+                Url = "https://www.youtube.com?v=aaa",
+                TitleAlias = "テスト",
+                Platform = LibraryItemPlatform.Youtube,
+                ErrorMessage = ""
+            };
+
+            //Act
+            var actResult = await controller.AddItem(model);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(actResult);
+            var modelResult = Assert.IsAssignableFrom<AddItemViewModel>(viewResult.Model);
+            Assert.Equal($"URL:「{model.Url}」から動画情報が取得できませんでした。", modelResult.ErrorMessage);
+
+        }
+
+        /// <summary>
+        /// アイテムライブラリへの追加画面：POST・失敗・動画URLのIDが不正
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task AddItem_ReturnAViewWithInvalidModelState_InvalidVideoUrl()
+        {
+            //Arrange
+            var store = new Mock<IUserStore<IdentityUser>>();
+            var userManagerMoq = new Mock<UserManager<IdentityUser>>(store.Object, null, null, null, null, null, null, null, null);
+            var configMoq = new Mock<IConfiguration>();
+            var itemLibRepo = new Mock<IItemLibraryRepository>();
+            var youtubeRepo = new Mock<IYoutubeDataRepository>();
+            youtubeRepo.Setup(repo => repo.GetYoutubeVideoInfoAsync(It.IsAny<string>()))
+                .Verifiable();
+
+            var controller = new PlayListController(
+                userManagerMoq.Object,
+                itemLibRepo.Object,
+                youtubeRepo.Object,
+                configMoq.Object);
+
+            var model = new AddItemViewModel()
+            {
+                Url = "hogehoge",
+                TitleAlias = "テスト",
+                Platform = LibraryItemPlatform.Youtube,
+                ErrorMessage = ""
+            };
+
+            //Act
+            var actResult = await controller.AddItem(model);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(actResult);
+            var modelResult = Assert.IsAssignableFrom<AddItemViewModel>(viewResult.Model);
+            Assert.Equal($"URL:「{model.Url}」から動画IDが取得できませんでした。", modelResult.ErrorMessage);
+
+        }
+
     }
 
    
