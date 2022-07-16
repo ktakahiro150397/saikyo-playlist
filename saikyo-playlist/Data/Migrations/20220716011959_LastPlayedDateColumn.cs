be476d82e2ddb7c3412df1e402d4102b25c94af2
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2022-07-16 10:23:59 +0900

    add lastplayeddatacolumn

---
+++ saikyo-playlist/Data/Migrations/20220716011959_LastPlayedDateColumn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace saikyo_playlist.Data.Migrations
+{
+    public partial class LastPlayedDateColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastPlayedDate",
+                table: "PlayListHeaders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "プレイリストが最後に再生された日付。");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastPlayedDate",
+                table: "PlayListHeaders");
+        }
+    }
+}
